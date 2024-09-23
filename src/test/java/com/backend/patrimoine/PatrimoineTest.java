e51d56519cbd4e079613b5f320fba303ec35aec6
herimaharavohaja <hei.haja.25@gmail.com>
2024-09-23 07:50:42 -0700

    feat: modification class

--- src/test/java/com/backend/patrimoine/PatrimoineTest.java
+++ src/test/java/com/backend/patrimoine/PatrimoineTest.java
@@ -17,7 +17,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 
-class PatrimoineServiceImplTest {
+class PatrimoineTest {
 
     @Mock
     private ObjectMapper objectMapper;
