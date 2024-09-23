07f0b21f03eaabffa5c9911d1cbc422d9833aa97
herimaharavohaja <hei.haja.25@gmail.com>
2024-09-23 07:08:54 -0700

    add Bean test

--- src/main/java/com/backend/patrimoine/PatrimoineApplication.java
+++ src/main/java/com/backend/patrimoine/PatrimoineApplication.java
@@ -1,7 +1,10 @@
 package com.backend.patrimoine;
 
+import com.backend.patrimoine.service.PatrimoineService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PatrimoineApplication {
@@ -10,4 +13,18 @@ public class PatrimoineApplication {
 		SpringApplication.run(PatrimoineApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(PatrimoineService patrimoineService) {
+		return new CommandLineRunner() {
+			@Override
+			public void run(String... args) throws Exception {
+				com.fresh.coding.patrimoineapi.model.Patrimoine foundPatrimoine = patrimoineService.findByName("Marie");
+				if (foundPatrimoine != null) {
+					System.out.println("Found Patrimoine: " + foundPatrimoine);
+				} else {
+					System.out.println("No Patrimoine found with the name 'Marie'.");
+				}
+			}
+		};
+	}
 }
